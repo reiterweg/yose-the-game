763c8d2ff5967405a9c5c3c4e02b82d7cf306c15
Alejandro Diaz <reiterweg@gmail.com>
2014-10-26 10:14:48 -0500

    World 1, Level 3

---
+++ src/com/reiterweg/ytg/service/Ping.java
@@ -0,0 +1,13 @@
+package com.reiterweg.ytg.service;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Ping extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("application/json");
+		resp.getWriter().println("{ \"alive\" : true }");
+	}
+}
